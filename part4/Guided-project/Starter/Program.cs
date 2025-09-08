00f2099904500d77d2968c6273661bb0401300f4
hailia sommerville <hailiassommerville@gmail.com>
2025-09-08 13:37:51 -0400

    search by pet characteristic added to case 2

--- part4/Guided-project/Starter/Program.cs
+++ part4/Guided-project/Starter/Program.cs
@@ -121,6 +121,7 @@ do
                     }
                 }
             }
+            
             Console.WriteLine("\n\rPress the Enter key to continue");
             readResult = Console.ReadLine();
 
@@ -140,6 +141,31 @@ do
                     dogCharacteristic = readResult.ToLower().Trim();
                 }
             }
+            
+            string dogDescription = "";
+            bool noMatchesDog = true;
+            // #6 loop through the ourAnimals array to search for matching animals
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 1].Contains("dog"))
+                {
+                    // #7 Search combined descriptions and report results
+                    dogDescription = ourAnimals[i, 4] + "\n" + ourAnimals[i, 5];
+                    if (dogDescription.Contains(dogCharacteristic))
+                    {
+                        Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
+                        Console.WriteLine(dogDescription);
+
+                        noMatchesDog = false;
+                    }
+                }
+            }
+
+            if (noMatchesDog)
+            {
+                Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+            }
+            
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
