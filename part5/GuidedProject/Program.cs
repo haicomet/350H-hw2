a6199e8b8c1ba426d9016fc95bed741354b76433
hailia sommerville <hailiassommerville@gmail.com>
2025-09-17 22:30:24 -0400

    unit 3 pseudocode

--- part5/GuidedProject/Program.cs
+++ part5/GuidedProject/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 
-string[] pettingZoo = 
+string[] pettingZoo =
 {
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
